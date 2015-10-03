d75706afc6dbd5b48bfbde142d713f9027623dae
Glauber Gallego <glauber.gallego@gmail.com>
2015-10-03 12:52:56 -0300

    shell to generate codedeployartifact

---
+++ gen_codedeploy.sh
@@ -0,0 +1,13 @@
+# generate source
+zip -r appfuse-v1-src.zip src/ pom.xml
+
+# generate binary
+rm -rf deploy
+cp -r src/main/deploy .
+mkdir deploy/content
+cp target/*.war deploy/content/ROOT.war
+cd deploy
+zip -r appfuse-v1.zip .
+mv appfuse-v1.zip ..
+cd ..
+
